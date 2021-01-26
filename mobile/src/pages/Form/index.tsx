da59a54fa9c350296a0104656dd9a16185cef2a0
Yan <yankaiquecosta.yk@gmail.com>
2021-01-26 13:18:38 -0300

    feat: listagem de usuários, modal para confirmação de exclusão

--- mobile/src/pages/Form/index.tsx
+++ mobile/src/pages/Form/index.tsx
@@ -1,94 +1,296 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { Alert, RefreshControl } from 'react-native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import * as Yup from 'yup';
+
 import { Form } from '@unform/mobile';
 
-import * as Yup from 'yup';
-import { Alert } from 'react-native';
-import { Container, Title, HeaderImage, Scroll } from './styles';
+import BlueWave from '../../assets/images/wave.png';
+import loading from '../../assets/gifs/loading.gif';
 
 import Input from '../../components/Input';
-import Button from '../../components/Button';
-import BlueWave from '../../assets/wave.png';
+import { useUserMethods } from '../../hooks/user/methods';
 import getValidationErrors from '../../utils/getValidationErrors';
-import { useUserMethods } from '../../hooks/user';
+import {
+  Scroll,
+  Page,
+  DeleteModalConfirmation,
+  ViewModal,
+  StyledViewModal,
+  TextModal,
+  ViewModalButton,
+  ModalButton,
+  TextButtonModal,
+  AdivisedText,
+  AdivisedTextArea,
+  Container,
+  HeaderImage,
+  Button,
+  TextButton,
+  LoadingGif,
+  Title,
+  UserContainerList,
+  UserInfoContainerList,
+  UserList,
+  UserInfoHeader,
+  ButtonIcon,
+  Icon,
+  UserInfoText,
+  CompanyInfoArea,
+  CompanyText,
+  DocumentText,
+} from './styles';
+
+import { database } from '../../services/connection';
+import InputMask from '../../components/InputMask';
+
+interface UserFormData {
+  name: string;
+  document: number;
+  type: 'individual' | 'business';
+}
 
 export default function SignIn() {
+  const [userInfo, setUserInfo] = useState([]);
+  const [disabledButton, setDisabledButton] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+  const [companyType, setCompanyType] = useState('');
+
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+  const [deletedItemId, setDeletedItemId] = useState('');
+
   const formRef = useRef(null);
-  const { post } = useUserMethods();
-  const handleSubmit = useCallback(async data => {
-    try {
-      const schema = Yup.object().shape({
-        name: Yup.string().required('Nome é obrigatório'),
-        document: Yup.number().required('Insira seu CPF ou CNPJ'),
-        type: Yup.string().required('Insira se ela é individual ou business'),
+  const { post, remove } = useUserMethods();
+
+  useEffect(() => {
+    database.collection('users').onSnapshot(query => {
+      const list = [];
+      query.forEach(doc => {
+        list.push({ ...doc.data().data, id: doc.id });
       });
+      setUserInfo(list);
+    });
+    setDisabledButton(false);
+  }, []);
 
-      await schema.validate(data, {
-        abortEarly: false,
-      });
+  useEffect(() => {
+    const formatedType = companyType.toLowerCase().trim();
+    setCompanyType(formatedType);
+  }, [companyType]);
 
-      const { name, document, type } = data;
+  const wait = timeout => {
+    return new Promise(resolve => {
+      setTimeout(resolve, timeout);
+    });
+  };
 
-      await post({
-        name,
-        document,
-        type,
-      });
-    } catch (err) {
-      if (err instanceof Yup.ValidationError) {
-        const errors = getValidationErrors(err);
-        formRef.current?.setErrors(errors);
+  const handleSubmit = useCallback(
+    async (data: UserFormData) => {
+      formRef.current.setErrors({});
+      setDisabledButton(true);
+      let min = 0;
+      try {
+        const formatedType = data.type.toLowerCase().trim();
+        if (formatedType === 'business') {
+          min = 14;
+        } else if (formatedType === 'individual') {
+          min = 11;
+        }
+        const schema = Yup.object().shape({
+          name: Yup.string()
+            .required('Nome é obrigatório')
+            .min(5, 'O nome deve conter mais de 5 letras')
+            .max(30, 'O nome deve conter menos de 30 letras'),
+          type: Yup.string()
+            .required('Insira se ela é individual ou business')
+            .max(10, 'O nome deve conter menos de 30 letras')
+            .test('tipo', 'Os tipos não batem', function (type: string) {
+              return (
+                type.toLowerCase().trim() === 'business' ||
+                type.toLowerCase().trim() === 'individual'
+              );
+            }),
+          document: Yup.number()
+            .required(`Insira ${min} digitos`)
+            .test('digito', 'Os digittos não batem', function (type) {
+              console.log(String(type).length);
+              return min === String(type).length;
+            }),
+        });
 
-        return;
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        const { name, document, type } = data;
+
+        await post({
+          name,
+          document,
+          type,
+        });
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current.setErrors(errors);
+          setDisabledButton(false);
+          return;
+        }
+        Alert.alert('Erro ao enviar', 'Envie seus dados novamente.');
       }
+      setDisabledButton(false);
+    },
+    [setDisabledButton, post],
+  );
 
-      Alert.alert(
-        'Erro ao enviar',
-        'Ocorreu um erro ao enviar seus dados, envie seus dados novamente.',
-      );
-    }
+  const handleDeleteModalUser = useCallback(
+    (id: string) => {
+      setDeletedItemId(id);
+      setModalIsVisible(true);
+    },
+    [setDeletedItemId, setModalIsVisible],
+  );
+
+  const handleConfirmDeleteUser = useCallback(async () => {
+    setModalIsVisible(false);
+    await remove(deletedItemId);
+  }, [remove, setModalIsVisible, deletedItemId]);
+
+  const onRefresh = React.useCallback(() => {
+    wait(60000).then(() => setRefreshing(false));
+  }, []);
+
+  const replaceCPF = useCallback((cpf: string) => {
+    return cpf.replace(/^(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+  }, []);
+
+  const replaceCNPJ = useCallback((cnpj: string) => {
+    return cnpj.replace(
+      /^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/g,
+      '$1.$2.$3/$4-$5',
+    );
   }, []);
 
   return (
-    <Scroll>
-      <HeaderImage source={BlueWave} />
-      <Container>
-        <Title style={{ fontSize: 30, fontWeight: 'bold' }}>Hey,</Title>
-        <Title>Insira seus dados</Title>
-        <Form style={{ marginTop: 10 }} ref={formRef} onSubmit={handleSubmit}>
-          <Input
-            placeholder="Nome"
-            name="name"
-            icon="user"
-            onSubmitEditing={() => {
-              formRef.current?.submitForm();
-            }}
-          />
-          <Input
-            keyboardType="numeric"
-            placeholder="CPF/CNPJ"
-            name="document"
-            icon="file-text"
-            onSubmitEditing={() => {
-              formRef.current?.submitForm();
-            }}
-          />
-          <Input
-            placeholder="Indivídual/Business"
-            name="type"
-            icon="briefcase"
-            onSubmitEditing={() => {
-              formRef.current?.submitForm();
-            }}
-          />
-          <Button
-            onPress={() => {
-              formRef.current?.submitForm();
-            }}
-          >
-            Registrar
-          </Button>
-        </Form>
-      </Container>
+    <Scroll
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+    >
+      <Page>
+        <DeleteModalConfirmation
+          visible={modalIsVisible}
+          animationType="slide"
+          transparent
+        >
+          <ViewModal>
+            <StyledViewModal>
+              <TextModal>Você deseja mesmo excluir esse usuário ?</TextModal>
+              <ViewModalButton>
+                <ModalButton
+                  onPress={() => handleConfirmDeleteUser()}
+                  backgroundColor="blue"
+                >
+                  <TextButtonModal>Sim</TextButtonModal>
+                </ModalButton>
+                <ModalButton
+                  onPress={() => setModalIsVisible(!modalIsVisible)}
+                  backgroundColor="red"
+                >
+                  <TextButtonModal>Não</TextButtonModal>
+                </ModalButton>
+              </ViewModalButton>
+            </StyledViewModal>
+          </ViewModal>
+        </DeleteModalConfirmation>
+
+        <HeaderImage source={BlueWave} />
+        <Container>
+          <Title style={{ fontSize: 30, fontWeight: 'bold' }}>Hey,</Title>
+          <Title>Insira seus dados</Title>
+          <Form style={{ marginTop: 10 }} ref={formRef} onSubmit={handleSubmit}>
+            <Input
+              placeholder="Nome"
+              name="name"
+              icon="user"
+              maxLength={30}
+              onSubmitEditing={() => {
+                formRef.current?.submitForm();
+              }}
+            />
+            <Input
+              placeholder="individual/business"
+              name="type"
+              icon="briefcase"
+              autoCapitalize="none"
+              maxLength={10}
+              onChangeText={text => setCompanyType(text)}
+              onSubmitEditing={() => {
+                formRef.current?.submitForm();
+              }}
+            />
+            <InputMask
+              placeholder={companyType === 'business' ? 'CNPJ' : 'CPF'}
+              name="document"
+              icon="file-text"
+              type={companyType === 'business' ? 'cnpj' : 'cpf'}
+              keyboardType="numeric"
+            />
+            <Button
+              disabled={disabledButton}
+              onPress={() => {
+                formRef.current?.submitForm();
+              }}
+            >
+              {!disabledButton && <TextButton>Registrar</TextButton>}
+
+              {disabledButton && (
+                <>
+                  <TextButton style={{ fontSize: 12, marginLeft: 3 }}>
+                    Enviando
+                  </TextButton>
+                  <LoadingGif source={loading} />
+                </>
+              )}
+            </Button>
+          </Form>
+
+          <AdivisedTextArea>
+            <AdivisedText>Veja alguns usuários</AdivisedText>
+            <FeatherIcon name="chevron-down" size={23} />
+          </AdivisedTextArea>
+          <UserContainerList>
+            {userInfo.length === 0 && (
+              <UserInfoText style={{ color: '#31353a' }}>
+                Nenhum registro
+              </UserInfoText>
+            )}
+            <UserList
+              nestedScrollEnabled
+              data={userInfo}
+              keyExtractor={info => info.id}
+              renderItem={({ item }) => (
+                <UserInfoContainerList backgroundColor={item.type}>
+                  <UserInfoHeader>
+                    <UserInfoText>{item.name}</UserInfoText>
+                    <ButtonIcon onPress={() => handleDeleteModalUser(item.id)}>
+                      <Icon name="x-circle" color="#fff" size={20} />
+                    </ButtonIcon>
+                  </UserInfoHeader>
+                  <CompanyInfoArea>
+                    <CompanyText>{item.type}</CompanyText>
+                    <DocumentText>
+                      {item.type === 'business'
+                        ? replaceCNPJ(item.document)
+                        : replaceCPF(item.document)}
+                    </DocumentText>
+                  </CompanyInfoArea>
+                </UserInfoContainerList>
+              )}
+            />
+          </UserContainerList>
+        </Container>
+      </Page>
     </Scroll>
   );
 }
--- mobile/src/pages/Form/styles.ts
+++ mobile/src/pages/Form/styles.ts
@@ -1,10 +1,77 @@
 import styled from 'styled-components/native';
 import { Platform } from 'react-native';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+
+interface BackgroundModelButtonProp {
+  backgroundColor: 'red' | 'blue';
+}
+
+interface BackgroundUserListProp {
+  backgroundColor: 'individual' | 'business';
+}
+
+export const Scroll = styled.ScrollView`
+  flex:1;
+`;
+
+export const Page = styled.SafeAreaView`
+
+`;
+
+export const DeleteModalConfirmation = styled.Modal`
+
+`;
+
+export const ViewModal = styled.View`
+  flex:1;
+  margin-bottom:60px;
+  justify-content:center;
+  align-items: center;
+  border-radius: 10px;
+  align-self: center;
+`;
+
+export const StyledViewModal = styled.View`
+  margin-bottom:30px;
+  justify-content:center;
+  align-items: center;
+  width: 90%;
+  height: 30%;
+  border-radius: 10px;
+  background-color: #1d75bd;
+  align-self: center;
+`;
+
+export const TextModal = styled.Text`
+  text-align: center;
+  font-size: 15px;
+  padding: 20px;
+  color: #fff;
+`;
+
+export const ViewModalButton = styled.View`
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+export const ModalButton = styled.TouchableOpacity<BackgroundModelButtonProp>`
+  background-color:${props => props.backgroundColor === 'blue' ? '#398efe' : '#ff0000'};
+  flex:1;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 15px;
+`;
+
+export const TextButtonModal = styled.Text`
+  text-align: center;
+  color: #f3f3f6;
+  font-size: 15px;
+`;
 
 export const Container = styled.View`
     flex:1;
     padding: 0 30px ${Platform.OS === 'android' ? 120 : 40}px;
-    margin-top: 80px;
+    margin-top: 100px;
 `;
 
 export const Title = styled.Text`
@@ -18,4 +85,103 @@ export const HeaderImage = styled.Image`
   position: absolute;
 `;
 
-export const Scroll = styled.ScrollView``;
+export const Button = styled.TouchableOpacity`
+  flex-direction: row;
+  width: 90px;
+  height: 40px;
+  margin-top: 10px;
+  background: #398efe;
+  border-radius: 6px;
+  align-self: flex-end;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const TextButton = styled.Text`
+  color: #FFFFFF;
+  font-size:18px;
+  font-weight: bold;
+`;
+
+export const LoadingGif = styled.Image`
+  height: 25px;
+  width:25px;
+  margin: 0 4px 0 2px;
+`;
+
+export const AdivisedTextArea = styled.View`
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  padding-top: 150px;
+`;
+
+export const AdivisedText = styled.Text`
+  font-size: 18px;
+  margin-right: 8px;
+  margin-bottom: 2px;
+`;
+
+export const UserContainerList = styled.View`
+  margin-top: 10px;
+  border-radius: 15px;
+  padding: 8px;
+  background-color: #f3f3f6;
+`;
+
+export const UserList = styled.FlatList`
+  flex: 1;
+`;
+
+export const UserInfoContainerList = styled.View<BackgroundUserListProp>`
+  height: 50px;
+  border-radius: 5px;
+  background-color: ${props=>props.backgroundColor === 'individual' ? '#ff6a00' : '#00b2ff'};
+  margin-top: 10px;
+`;
+
+export const UserInfoHeader = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const UserInfoText = styled.Text`
+  color: #fff;
+  margin-left: 10px;
+  margin-top: 3px;
+`;
+
+export const ButtonIcon = styled.TouchableOpacity`
+  position: absolute;
+  right: 0;
+  margin-right: 3px;
+  margin-top: 3px
+`;
+
+export const Icon = styled(FeatherIcon)`
+
+`;
+
+export const CompanyInfoArea = styled.View`
+  justify-content: space-between;
+  flex-direction: row;
+`;
+
+export const CompanyText = styled.Text`
+  font-weight: bold;
+  font-size: 12px;
+  color: #fff;
+  margin-left: 10px;
+  margin-right: 10px;
+  margin-top: 5px;
+`;
+
+export const DocumentText = styled.Text`
+  font-style: italic;
+  font-size: 10px;
+  color: #fff;
+  margin-left: 10px;
+  margin-right: 10px;
+  margin-top: 8px;
+`;
+
